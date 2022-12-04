914dbdc99ac9e8e7caec90f1874c519b6cf0530a
Van-Ivan <pibo0121016@sibupk.su>
2022-12-04 14:01:03 +0700

    Сделал 2 задание

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            string[] input = File.ReadAllLines(@"D:\github\Lab5\Text1.txt"); //чтение массива строк
+            Regex regex1 = new Regex(@"а");
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Console.WriteLine(input[i]);
+                
+                Console.WriteLine(regex1.IsMatch(input[i]));
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
