0a0998ebb18952e65bc058dc18c3a2b9feebe472
Van-Ivan <pibo0121016@sibupk.su>
2022-12-09 16:34:05 +0700

    Закончена основная часть

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Lab5
 {
@@ -17,7 +20,6 @@ namespace Lab5
             Regex regex1 = new Regex(@"^а$"); 
             Regex regex2 = new Regex(@"[а]{6}");
             Regex regex3 = new Regex(@"а\s[а]{2}\s");
-            Regex regex4 = new Regex(@"[а\s[а]{2}\s^а$[а]{6}]");
 
             
             for (int i = 0; i < input.Length; ++i)
@@ -28,14 +30,83 @@ namespace Lab5
                 {
                     Console.Write(" - Строка подходит");
                 }
-                Console.WriteLine();
+                Console.WriteLine(String.Empty);
             }
             
 
             //Задание 2
-            Console.WriteLine("Задание 2");
-            string str = Console.ReadLine();
-            Console.WriteLine(new Regex(@"[0-9a-zA-Z]{5,}").IsMatch(str));
+            Console.WriteLine("\nЗадание 2");
+
+            Console.WriteLine("Введи не менее 5 алфавитно-цифровых символов:");
+            string str = Console.ReadLine();                                //Считываем с консоли текст
+            Console.WriteLine(new Regex(@"[0-9a-zA-Z]{5,}").IsMatch(str)); //Выводим правильный ли он
+
+            //Задание 3
+            Console.WriteLine("\nЗадание 3");
+            input = File.ReadAllLines(@"D:\github\Lab5\Text3.txt");
+            foreach (string line in input)
+            {
+                Console.Write(line + " - ");
+                Console.WriteLine(new Regex(@"^[a-zA-Z]\w{3,20}@[a-z]{2,10}\.[a-z]{2,5}").IsMatch(line));
+            }
+
+            //Задание 4
+            Console.WriteLine("\nЗадание 4");
+            Console.OutputEncoding = Encoding.UTF8; 
+
+            input = File.ReadAllLines(@"D:\github\Lab5\text4.txt");
+
+            var patternFor4Exs = new Regex(@"[А-Я][а-я]+([-][А-Я][а-я]+)?[:]\s(широта\s)?[0-9]{1,2}(\.[0-9]+)?,?\s(долгота\s)?[0-9]{1,2}(\.[0-9]+)?");
+            //Паттерн для поиска подходящих строк
+            var city = new Regex(@"[А-Я][а-я]+([-][А-Я][а-я]+)?");
+            //Паттерн для поиска названия города
+            var digit = new Regex(@"[0-9]{1,2}(\.[0-9]+)?");
+            //Паттерн для поиска значений широты и долготы
+
+            foreach (string line in input)
+            {
+                if(patternFor4Exs.IsMatch(line))
+                    Console.WriteLine(city.Match(line) + " Ш: " + digit.Match(line) + " Д: " + digit.Match(line));
+            }
+            //Задание 5
+            Console.WriteLine("\nЗадание 5");
+
+            input = File.ReadAllLines(@"D:\github\Lab5\Лабораторная работа 5 - testData.xml");
+
+            Console.WriteLine("Пункт а");
+
+            foreach (string line in input)
+            {       //Ищем строки, в которых прописаны координаты, и выводим их на консоль
+                if (patternFor4Exs.IsMatch(line))
+                {
+                    Console.WriteLine(city.Match(line) + " Ш: " + digit.Match(line) + " Д: " + digit.Match(line)); 
+                }
+            }
+
+            Console.WriteLine("Пункт b,c,d");
+
+            Regex nodeSpace = new Regex(@"\s+<node1>"); //Паттерн для поиска узлов 1 уровня
+            Regex tags1 = new Regex(@"<node\d>");  //Паттерн для поиска открывающих тегов любого уровня
+            Regex tags2 = new Regex(@"</node\d>"); //Паттерн для поиска закрывающих тегов любого уровня
+            string buff;        //Строка для хранения изменяемой и выводимой строки
+            string tag1;        //Строка для хранения открывающего тега в строке
+            string tag2;        //Строка для хранения закрывающего тега в строке
+
+            foreach (string line in input)
+            {
+                buff = Regex.Replace(line, @"^\d+.", String.Empty); //убираем нумерацию
+                if (nodeSpace.IsMatch(buff))
+                {
+                    buff = Regex.Replace(buff, @"\s+<node1>", "..<node1>"); //пробелы заменяем на 2 точки (чтоб видно лучше было) в node1
+                }
+
+                //Ищем теги узлов
+                tag1 = tags1.Match(line).Value;                          
+                tag2 = tags2.Match(line).Value;                            
+                if (tag1 != tag2)                                     //Если они не совпадают то
+                    buff = Regex.Replace(buff, @"</node\d>", tag1);  //Заменяем неправильный закрывающийся тег
+                Console.WriteLine(buff);
+            }
         }
     }
 }
